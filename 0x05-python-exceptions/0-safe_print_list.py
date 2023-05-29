1437c0b479407175ce73acd34bb4a084a02c9cb4
Risperwangui <resperwangui68@gmail.com>
2023-05-29 08:59:45 -0700

    writing a function that prints x elements of a lsit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    re = 0
+    for i in range (x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            re = re + 1
+        except IndexError:
+            break
+    print("")
+    return (re)
