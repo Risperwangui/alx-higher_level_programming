d66a9c0db7276dbeebaff4f21fab87d518682073
Risperwangui <resperwangui68@gmail.com>
2023-09-04 05:14:33 -0700

    fixing bugs

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,7 +7,7 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    req = request.get(url)
+    req = requests.get(url)
     if req.status_code >= 400:
         print("Error code: {}".format(req.status_code))
     else:
