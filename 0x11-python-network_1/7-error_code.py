19fc4b86ab234434d93404da5a26ad9576938ab2
Risperwangui <resperwangui68@gmail.com>
2023-09-04 05:02:24 -0700

    writing a script that takes in a url

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A script that takes in a url,sends a request to the url"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    req = request.get(url)
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
