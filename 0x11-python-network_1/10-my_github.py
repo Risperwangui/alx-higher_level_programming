441308a883831e27bf9cbd30c712390ae2f368a2
Risperwangui <resperwangui68@gmail.com>
2023-09-04 07:58:55 -0700

    fixing errors

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,5 +8,5 @@ from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    re = request.get("https://api.github.com/user", auth=auth)
+    re = requests.get("https://api.github.com/user", auth=auth)
     print(re.json().get("id"))
