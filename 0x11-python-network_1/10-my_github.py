7bec86330daa8ea9287c2813e969f69b0a4f4191
Risperwangui <resperwangui68@gmail.com>
2023-09-04 07:52:58 -0700

    writing a python script that takes your github credentials

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""writing a python script that takes your Github credentials"""
+
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    re = request.get("https://api.github.com/user", auth=auth)
+    print(re.json().get("id"))
