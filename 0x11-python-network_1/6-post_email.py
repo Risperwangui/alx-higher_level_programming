84927b52d3ed87af313b9422176763f402c0d3e4
Risperwangui <resperwangui68@gmail.com>
2023-09-04 05:09:45 -0700

    fixing errors

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """A script that takes in a URL"""
 import sys
-import urllib.request
+import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
+    email = sys.argv[2]
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    data = {'email': email}
+
+    res = requests.post(url, data=data)
+    print(res.text)
